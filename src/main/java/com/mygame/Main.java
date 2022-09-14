45cce38d0d49941f714b559edba9bc337b2eed99
ArtemPopof <deviling42@gmail.com>
2022-09-14 21:23:23 +0300

    init jme project

--- src/main/java/com/mygame/Main.java
+++ src/main/java/com/mygame/Main.java
@@ -1,8 +1,20 @@
 package com.mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.bounding.BoundingBox;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.input.ChaseCamera;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.Camera;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
@@ -13,6 +25,16 @@ import com.jme3.scene.shape.Box;
  * @author normenhansen
  */
 public class Main extends SimpleApplication {
+    
+    private final Vector3f playerWalkDirection = Vector3f.ZERO;
+    private boolean left = false;
+    private boolean right = false;
+    private boolean up = false;
+    private boolean down = false;
+    private CharacterControl playerControl;
+    private Camera camera;
+    private Vector3f camDir;
+    private Vector3f camLeft;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -21,19 +43,90 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        var scene = assetManager.loadModel("Scenes/yivysky-osm.j3o");
+        
+        var bulletAppState = new BulletAppState();
+        bulletAppState.setDebugEnabled(true);
+        stateManager.attach(bulletAppState);
+                
+        setupLight();
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        rootNode.attachChild(scene);
+        var floor = rootNode.getChild("Floor");
+        var floorControl = new RigidBodyControl(0);
+        floor.addControl(floorControl);
+        bulletAppState.getPhysicsSpace().add(floorControl);
+        
+        var player = rootNode.getChild("Player");                                                               
+        var box = (BoundingBox) player.getWorldBound();
+        var radius = box.getXExtent();
+        var height = box.getYExtent();
+        var shape = new CapsuleCollisionShape(radius, height);
+        playerControl = new CharacterControl(shape, 1.0f);
+        player.addControl(playerControl);
+        
+        bulletAppState.getPhysicsSpace().add(playerControl);
+        
+        this.getFlyByCamera().setEnabled(false);
+        camera = getCamera();
+        var chaseCamera = new ChaseCamera(camera, player, inputManager);
+        
+        addInputMappings();
+                                                                                         
+    }
+    
+    private void setupLight() {
+        DirectionalLight sun = new DirectionalLight();
+        sun.setColor(ColorRGBA.White);
+        sun.setDirection(new Vector3f(-.5f,-.5f,-.5f).normalizeLocal());
+        rootNode.addLight(sun);
     }
 
+    private void addInputMappings() {
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        inputManager.addListener(createInputListener(), "Up", "Down", "Left", "Right", "Jump");
+    }
+    
+    private ActionListener createInputListener() {
+        return (name, keyPressed, tpf) -> {
+            if (name.equals("Up") ) {
+                up = keyPressed;
+            } else if (name.equals("Down")) {
+                down = keyPressed;
+            } else if (name.equals("Left")) {
+                left = keyPressed;
+            } else if (name.equals("Right")) {
+                right = keyPressed;
+            } else if (name.equals("Jump")) {
+                playerControl.jump();
+            }
+        };
+    }
+    
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        camDir = camera.getDirection().clone();
+        camLeft = camera.getLeft().clone();
+        
+        camDir.y = 0;
+        camLeft.y = 0;
+        
+        camDir.normalizeLocal();
+        camLeft.normalizeLocal();
+        
+        playerWalkDirection.set(0, 0, 0);
+        
+        if (left) playerWalkDirection.addLocal(camLeft);
+        if (right) playerWalkDirection.addLocal(camLeft.negate() );
+        if (up) playerWalkDirection.addLocal(camDir);
+        if (down) playerWalkDirection.addLocal(camDir.negate());
+        
+        playerWalkDirection.multLocal(10f).multLocal(tpf);
+        playerControl.setWalkDirection(playerWalkDirection);
     }
 
     @Override
